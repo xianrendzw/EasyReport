732147e404ab70ecdfa0fa8c551d58b06226bd33
TomDeng <14068728@qq.com>
2016-10-12 15:54:12 +0800

    update

--- easyreport-engine/src/main/java/com/easytoolsoft/easyreport/engine/util/JdbcUtils.java
+++ easyreport-engine/src/main/java/com/easytoolsoft/easyreport/engine/util/JdbcUtils.java
@@ -21,7 +21,7 @@ public class JdbcUtils {
     private static Map<String, DataSource> dataSourceMap = new ConcurrentHashMap<>(100);
 
     public static DataSource getDataSource(ReportDataSource rptDs) {
-        //用户数据源用户名,密码,jdbcUrl做为key
+        //用数据源用户名,密码,jdbcUrl做为key
         String key = String.format("%s|%s|%s", rptDs.getUser(), rptDs.getPassword(), rptDs.getJdbcUrl()).toLowerCase();
         DataSource dataSource = dataSourceMap.get(key);
         if (dataSource == null) {
