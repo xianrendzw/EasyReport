1db53cdff1aa4bcbe2cd5cad7e7ac4de34c17a5d
vvchina <jilindaxue11111@163.com>
2016-12-22 02:18:40 +0800

    commit

--- easyreport-database/src/main/java/DataBase.java
+++ easyreport-database/src/main/java/DataBase.java
@@ -8,42 +8,44 @@ public class DataBase {
     static DB db = null;
 
     public static void main(String[] args) {
+    	boolean init =false;
         if (!new File("db/dbdir").exists()) {
-            long startTime = System.currentTimeMillis();
-            try {
-                ZipInputStream Zin = new ZipInputStream(new FileInputStream("db/dbdir.zip"));
-                BufferedInputStream Bin = new BufferedInputStream(Zin);
-                String Parent = "db"; // 输出路径（文件夹目录）
-                File Fout = null;
-                ZipEntry entry;
-                try {
-                    while ((entry = Zin.getNextEntry()) != null && !entry.isDirectory()) {
-                        Fout = new File(Parent, entry.getName());
-                        if (!Fout.exists()) {
-                            (new File(Fout.getParent())).mkdirs();
-                        }
-                        FileOutputStream out = new FileOutputStream(Fout);
-                        BufferedOutputStream Bout = new BufferedOutputStream(out);
-                        int b;
-                        while ((b = Bin.read()) != -1) {
-                            Bout.write(b);
-                        }
-                        Bout.close();
-                        out.close();
-                        System.out.println(Fout + "解压成功");
-                    }
-                    Bin.close();
-                    Zin.close();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            } catch (FileNotFoundException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            long endTime = System.currentTimeMillis();
-            System.out.println("耗费时间： " + (endTime - startTime) + " ms");
+        	init=true;
+//            long startTime = System.currentTimeMillis();
+//            try {
+//                ZipInputStream Zin = new ZipInputStream(new FileInputStream("db/dbdir.zip"));
+//                BufferedInputStream Bin = new BufferedInputStream(Zin);
+//                String Parent = "db"; // 输出路径（文件夹目录）
+//                File Fout = null;
+//                ZipEntry entry;
+//                try {
+//                    while ((entry = Zin.getNextEntry()) != null && !entry.isDirectory()) {
+//                        Fout = new File(Parent, entry.getName());
+//                        if (!Fout.exists()) {
+//                            (new File(Fout.getParent())).mkdirs();
+//                        }
+//                        FileOutputStream out = new FileOutputStream(Fout);
+//                        BufferedOutputStream Bout = new BufferedOutputStream(out);
+//                        int b;
+//                        while ((b = Bin.read()) != -1) {
+//                            Bout.write(b);
+//                        }
+//                        Bout.close();
+//                        out.close();
+//                        System.out.println(Fout + "解压成功");
+//                    }
+//                    Bin.close();
+//                    Zin.close();
+//                } catch (IOException e) {
+//                    // TODO Auto-generated catch block
+//                    e.printStackTrace();
+//                }
+//            } catch (FileNotFoundException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            }
+//            long endTime = System.currentTimeMillis();
+//            System.out.println("耗费时间： " + (endTime - startTime) + " ms");
         }
         DBConfigurationBuilder config = DBConfigurationBuilder.newBuilder();
         config.setDataDir("db/dbdir");
@@ -60,6 +62,20 @@ public class DataBase {
                 // in case we need another DB, here's how to create it first
                 db.createDB(dbName);
             }
+             
+            System.out.println(System.getProperty("user.dir") );
+            
+            if(init){
+            	System.out.println("start import data, that will cost a lot time, please wait...");
+            	System.out.println("table easyResport2 start");
+            	db.run("source "+System.getProperty("user.dir").replaceAll("\\\\","/")+"/src/main/resources/db/easyreport2.sql;");
+            	System.out.println("table easyResport2 end");
+            	System.out.println("table china_weather_air_mysql start");
+            	db.run("source "+System.getProperty("user.dir").replaceAll("\\\\","/")+"/src/main/resources/db/china_weather_air_mysql.sql;");
+            	System.out.println("table china_weather_air_mysql end");
+            	db.run("create user data identified by 'data123';");
+            }
+            System.out.println("end init");
 
             // db.source("db/easyreport2.sql", "root", null, dbName);
             // db.source("db/china_weather_air_mysql.sql", "root", null, dbName);
