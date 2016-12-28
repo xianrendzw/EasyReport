1195f067f1be4ee8256675b6f29d1800dcc709e7
vvchina <jilindaxue11111@163.com>
2016-12-28 09:07:08 +0800

    scheduler spring framework.

--- easyreport-scheduler/src/main/java/com/easytoolsoft/easyreport/scheduler/QuartzManager.java
+++ easyreport-scheduler/src/main/java/com/easytoolsoft/easyreport/scheduler/QuartzManager.java
@@ -1,6 +1,5 @@
 package com.easytoolsoft.easyreport.scheduler;
 
-import java.text.ParseException;
 import java.util.List;
 
 import org.quartz.CronScheduleBuilder;
@@ -14,17 +13,22 @@ import org.quartz.TriggerBuilder;
 import org.quartz.TriggerKey;
 import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import com.easytoolsoft.easyreport.data.schedule.po.Task;
 import com.easytoolsoft.easyreport.domain.schedule.service.ITaskService;
+import com.easytoolsoft.easyreport.domain.schedule.service.impl.TaskService;
 
+@Component
+@Scope(value = "singleton")
 public class QuartzManager {
-    SchedulerFactory sf = new StdSchedulerFactory();
-
+    private SchedulerFactory sf = new StdSchedulerFactory();
     @Autowired
-    private ITaskService taskService;
+    private TaskService taskService;
+    
 
-    public void reScheduleJob() throws Exception, ParseException {
+    public void reScheduleJob() {
         List<Task> tasks = taskService.getAll();
         if (tasks != null && tasks.size() > 0) {
             for (Task task : tasks) {
@@ -32,11 +36,17 @@ public class QuartzManager {
             }
         }
     }
+    
+    public void triggerTask(int taskid){
+    	new ReportJob().execute(taskid);
+    }
+    
+    
 
     public boolean configQuatrz(Task task) {
         boolean result = false;
         try {
-            CronTrigger trigger = (CronTrigger) sf.getScheduler().getTrigger(new TriggerKey(task.getId()+"", Scheduler.DEFAULT_GROUP));
+            CronTrigger trigger = (CronTrigger) sf.getScheduler().getTrigger(new TriggerKey("Trigger_"+task.getId(), Scheduler.DEFAULT_GROUP));
             if (trigger != null) {
                 change(task,  trigger);
             } else {
@@ -54,11 +64,11 @@ public class QuartzManager {
         if(task.getId()>0){
             if (!trigger.getCronExpression().equalsIgnoreCase(task.getCronExpr())) {
                 ((org.quartz.impl.triggers.CronTriggerImpl) trigger).setCronExpression(task.getCronExpr());
-                sf.getScheduler().rescheduleJob(new TriggerKey(task.getId()+"", Scheduler.DEFAULT_GROUP), trigger);
+                sf.getScheduler().rescheduleJob(new TriggerKey("Trigger_"+task.getId(), Scheduler.DEFAULT_GROUP), trigger);
             }
         }else {
-            sf.getScheduler().pauseTrigger(new TriggerKey(task.getId()+"", Scheduler.DEFAULT_GROUP));
-            sf.getScheduler().unscheduleJob(new TriggerKey(task.getId()+"", Scheduler.DEFAULT_GROUP));
+            sf.getScheduler().pauseTrigger(new TriggerKey("Trigger_"+task.getId(), Scheduler.DEFAULT_GROUP));
+            sf.getScheduler().unscheduleJob(new TriggerKey("Trigger_"+task.getId(), Scheduler.DEFAULT_GROUP));
             sf.getScheduler().deleteJob(trigger.getJobKey());
         }
 
@@ -74,12 +84,12 @@ public class QuartzManager {
 
         
         JobDetail job = JobBuilder.newJob(ReportJob.class)
-                .withIdentity("job1", "group1")
+                .withIdentity("Job_"+task.getId(), Scheduler.DEFAULT_GROUP)
                 .build();
 
         CronTrigger trigger = TriggerBuilder.newTrigger()
-                .withIdentity("trigger1", "group1")
-                .withSchedule(CronScheduleBuilder.cronSchedule("0/20 * * * * ?"))
+                .withIdentity("Trigger_"+task.getId(), Scheduler.DEFAULT_GROUP)
+                .withSchedule(CronScheduleBuilder.cronSchedule(task.getCronExpr()))
                 .build();
 
             sf.getScheduler().scheduleJob(job, trigger);
--- easyreport-scheduler/src/main/java/com/easytoolsoft/easyreport/scheduler/ReportJob.java
+++ easyreport-scheduler/src/main/java/com/easytoolsoft/easyreport/scheduler/ReportJob.java
@@ -4,11 +4,21 @@ import java.util.Date;
 
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.JobKey;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.easytoolsoft.easyreport.data.schedule.po.Task;
+import com.easytoolsoft.easyreport.domain.schedule.service.ITaskService;
 
 public class ReportJob implements org.quartz.Job{
+    @Autowired
+    private ITaskService taskService;
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        JobKey jobKey = context.getJobDetail().getKey();
-        System.out.println("SimpleJob says: " + jobKey + " executing at " + new Date());
+        String  taskid = context.getJobDetail().getKey().getName().substring(4);
+        this.execute(Integer.parseInt(taskid));
+    }
+    
+    public void execute(int taskid){
+    	 Task task = taskService.getById(taskid);
+         System.out.println("SimpleJob says: " + task + " executing at " + new Date());
     }
 }
