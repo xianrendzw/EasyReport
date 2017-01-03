99bba7b6b1244a1150339f60ec2869200daae982
vvchina <jilindaxue11111@163.com>
2017-01-04 00:56:36 +0800

    database create

--- easyreport-database/src/main/resources/db/easyreport2.sql
+++ easyreport-database/src/main/resources/db/easyreport2.sql
@@ -15,6 +15,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE IF NOT EXISTS `easyreport2`;
+USE easyreport2;
 --
 -- Table structure for table `ezrpt_member_module`
 --
