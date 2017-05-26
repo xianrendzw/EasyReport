25496f631727f1aa4b8da70408fbbde55b4b5735
zhiwei.deng@okcoin.com <zhiwei.deng@okcoin.com>
2017-05-26 12:24:32 +0800

    refactor

---
+++ easyreport-common/src/main/java/com/easytoolsoft/easyreport/common/enums/BizErrorCode.java
@@ -0,0 +1,37 @@
+package com.easytoolsoft.easyreport.common.enums;
+
+import com.easytoolsoft.easyreport.support.enums.ErrorCode;
+import com.easytoolsoft.easyreport.support.i18n.LocaleUtils;
+
+/**
+ * @author Tom Deng
+ * @date 2017-05-01
+ **/
+public enum BizErrorCode implements ErrorCode {
+    UNKNOWN_ERROR(-1, "error.code.unknow"),
+    BAB_CREDENTIALS(11000, "error.code.11000"),
+    REFRESH_TOKEN_FAILURE(11001, "error.code.11001");
+
+    private int code;
+    private String message;
+
+    BizErrorCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public int getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String getMessage() {
+        return LocaleUtils.getMessage(this.message);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getCode() + "]" + this.getMessage();
+    }
+}
